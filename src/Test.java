952111458517aa90cac0f35b2e3c4c46acee9c67
wanghaizhou <1075444875@qq.com>
2014-07-04 21:00:33 +0800

    change the test class for test

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,11 @@
+import sql.util.SqlUtilImp;
+
 /**
  * Created by wanghaizhou on 2014/7/4.
  */
 public class Test {
     public void  add(){
         System.out.println("你好");
+        new SqlUtilImp().test();
     }
 }
