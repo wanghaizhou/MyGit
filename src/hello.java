e49d30e3313722fb4841ecff021867e17688b9b8
wanghaizhou <1075444875@qq.com>
2014-07-04 22:22:00 +0800

    create Server

--- src/hello.java
+++ src/hello.java
@@ -4,8 +4,28 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("nihao");
+        new Hello().domyself();
+        new Hello().excute();
     }
     public void excute(){
         System.out.println("");
+        excute1();
+    }
+    public void excute1(){
+        System.out.println("");
+        excute2();
+    }
+    public void excute2(){
+        System.out.println("");
+        System.out.println("s");
+        System.out.println("s2");
+    }
+    public void domyself(){
+        System.out.println("haohaoxians");
+        excute2();;
+    }
+    public void domyself2(){
+        System.out.println("haohaoxians");
+        excute2();;
     }
 }
